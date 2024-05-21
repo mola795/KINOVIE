307fedbff0dfbf8da0f5b2f78098b1e484006539
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-21 15:04:55 +0900

    search basic setup

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,7 @@
 class SearchController < ApplicationController
+  def index
+    @query = params[:query]
+    @titles = Title.where("name ILIKE ?", "%#{@query}%")
+    @genres = Genre.where("name ILIKE ?", "%#{@query}%")
+  end
 end
