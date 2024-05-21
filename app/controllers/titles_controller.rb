07c778d235fdf660156c84f9bbff559c7f63bf39
Moritz Lange <moritz.langebuy@web.de>
2024-05-21 16:21:47 +0900

    created the show page with api calls

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -5,9 +5,17 @@ class TitlesController < ApplicationController
 
   def show
     @title = Title.find(params[:id])
+    tmdb_api_key = ENV['TMDB_API_KEY']
+    tmdb_api = TmdbApi.new(tmdb_api_key)
+
+    @movie_details = tmdb_api.fetch_movie_details(@title.tmdb_id)
+    @description = @movie_details['overview']
+    @genres = @movie_details['genres'].map { |genre| genre['name'] }.join(", ")
+    @cast = tmdb_api.fetch_movie_cast(@title.tmdb_id).map { |cast_member| cast_member['name'] }.join(", ")
   end
 
   def popular
     @titles = Title.where(media_type: 'movie').order('created_at DESC').limit(20)
+    @total_titles_count = Title.count
   end
 end
