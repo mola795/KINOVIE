dbe0fba2be00c0de6b4cf627fe804604cb6d5e82
Andrew Attard <a2attard@gmail.com>
2024-05-21 16:31:51 +0900

    Commented seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,33 +3,29 @@ require 'faker'
 
 puts "Cleaning the DB..."
 # Destroy all records for seeded objects
-# Object.destroy_all
+Streaming.destroy_all
+Title.destroy_all
+Service.destroy_all
 
+# Structure for utilization of HTTParty: https://github.com/jnunemaker/httparty
 class WatchModeApi
   include HTTParty
   base_uri 'https://api.watchmode.com/v1'
 
+  # Method to get all available sources (Netflix, AppleTV, Prime, etc.).
+  # Can be filtered by region(s), given a one or more 2-char country code ('US,CA,JP')
   def get_sources(regions)
-    # Where regions is a comma separated string of 2 character country codes
-    # available from the watchmode /regions endpoint
     api_key = 'Wa0xNagkzZLawLxWCcarWIGDwFW6bILgqpJrokZf'
     self.class.get("/sources/?apiKey=#{api_key}&regions=#{regions}")
   end
 
-  # def get_source_details(source_id)
-  #   api_key = 'Wa0xNagkzZLawLxWCcarWIGDwFW6bILgqpJrokZf'
-  #   sources = self.class.get("/sources/?apiKey=#{api_key}&regions=#{regions}")
-  # end
-
+  # Method to get the details of all available sources for a given title
+  # 'Where can I watch Fallout?'
   def get_title_details(title_id, regions)
-    # Where regions is a comma separated string of 2 character country codes
-    # available from the watchmode /regions endpoint
     api_key = 'Wa0xNagkzZLawLxWCcarWIGDwFW6bILgqpJrokZf'
     self.class.get("/title/#{title_id}/sources/?apiKey=#{api_key}&regions=#{regions}")
   end
 end
-
-watchmode_api = WatchModeApi.new
 
 class TMDBApi
   include HTTParty
@@ -45,7 +41,9 @@ class TMDBApi
 end
 
 def seed_popular_movies(api)
+  # Return a list of popular movies
   movies = api.fetch_popular_movies['results']
+  # For each movie returned, create a record with the extracted details
   movies.each do |movie|
     title = Title.create(
       name: movie['title'],
@@ -54,15 +52,23 @@ def seed_popular_movies(api)
       tmdb_id: movie['id'],
       poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
     )
+    # For each title, assign its tmdb_id to use in the Watchmode Request
     tmdb_id = title.tmdb_id
-
-    streaming_sources = watchmode_api.get_title_details(tmdb_id, '')
+    # Create a new instance of the Watchmode API request
+    watchmode_api = WatchModeApi.new
+    # Get all streaming sources available for the current title in the US
+    streaming_sources = watchmode_api.get_title_details(tmdb_id, 'US')
+    # Selecting only the HD sources, find the corresponding service from our existing
+    # DB of streaming services based on name
     streaming_sources.select{|streaming_src| streaming_src["format"] == 'HD'}.each do |streaming_src|
       service = Service.where(name: streaming_src['name']).first
+      # Create a new streaming record, pulling url from the current streaming source
       streaming = Streaming.new(
-        url: streaming_src['web_url'],
-        # service_id: service.id
+        url: streaming_src['web_url']
       )
+      # When assigning reference, do it outside of record creation, then save
+      # The below ties our newly created streaming record to its associated
+      # service & title
       streaming.service = service
       streaming.title = title
       streaming.save
@@ -70,12 +76,13 @@ def seed_popular_movies(api)
   end
 end
 
-# Seed all available sources
-services = watchmode_api.get_sources('')
+watchmode_api = WatchModeApi.new
+# Seed all available sources (within the US for now)
+services = watchmode_api.get_sources('US')
 services.each do |service|
   Service.create(
-    name: service.name,
-    watchmode_id: service.id
+    name: service['name'],
+    watchmode_id: service['id']
   )
 end
 
