692968fb8e0e396db25b5666018a62b8a54790db
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-21 17:04:28 +0900

    fix

--- lib/tmdb_api.rb
+++ lib/tmdb_api.rb
@@ -1,3 +1,4 @@
+# lib/template/tmdb_api.rb
 require 'httparty'
 
 class TmdbApi
@@ -14,5 +15,10 @@ class TmdbApi
 
   def fetch_movie_cast(movie_id)
     self.class.get("/movie/#{movie_id}/credits", query: { api_key: @api_key, language: 'en-US' })['cast']
+  end
+
+  def search_movies(query, limit = 5)
+    response = self.class.get("/search/movie", query: { api_key: @api_key, query: query, language: 'en-US' })
+    response['results'].first(limit) # limit the number of results
   end
 end
