ffc35279e55d5e4b9770eb8639b2fea117444fdb
Moritz Lange <moritz.langebuy@web.de>
2024-05-21 11:43:24 +0900

    validations added

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+  has_many :people_titles
+  has_many :titles, through: :people_titles
+
+  validates :name, presence: true
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,6 @@
 class Service < ApplicationRecord
+  has_many :streamings
+  has_many :titles, through: :streamings
+
+  validates :name, presence: true
 end
