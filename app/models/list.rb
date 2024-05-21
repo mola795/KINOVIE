82834fd89523ecc733ae76d4436aa3fad4d61316
Andrew Attard <a2attard@gmail.com>
2024-05-21 12:04:02 +0900

    association/validation adjustments

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,12 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :list_items
 
+  # before_validation :set_defaults
   validates :name, presence: true
+
+  # private
+
+  # def set_defaults
+  #   status = "Private" if status.blank?
+  # end
 end
