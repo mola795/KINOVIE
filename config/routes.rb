df0cf3d8b6449bb13f3f67dabd1d3edc9cc3f3a9
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-21 13:39:00 +0900

    initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get '/profile', to: 'pages#profile', as: :profile
   get '/users/:username', to: 'pages#username', as: :username
+  get 'search', to: 'search#index', as: 'search'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
