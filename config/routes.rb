b601fd327f48459986a7302dcfac42992ba71d7c
Andrew Attard <a2attard@gmail.com>
2024-05-21 10:45:31 +0900

    Added routes for currently prioritized features

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :genres, only: %i[index show]
+  resources :lists, only: %i[show new create]
+  resources :titles, only: %i[show]
+  resources :list_items, only: %i[new create destroy]
+
+  get '/profile', to: 'pages#profile', as: :profile
+  get '/users/:username', to: 'pages#username', as: :username
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
