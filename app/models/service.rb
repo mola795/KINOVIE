5f2a40ba2267763c1198283c5da95a2fe87a859e
Andrew Attard <a2attard@gmail.com>
2024-05-23 15:43:06 +0900

    Groups and displays streaming URLs for each title by type (Buy, Rent, Paid Sub, Ad Supported

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,9 @@
 class Service < ApplicationRecord
   has_many :streamings, dependent: :destroy
-  has_many :titles, through: :streamings
+  # Although a title can have many streamings & services, the title ID is unique.
+  # Specifying the distinct method will prevent the same title from being called
+  # more than once when selecting titles with service or streaming joins
+  has_many :titles, -> { distinct }, through: :streamings
 
   validates :name, presence: true
 end
