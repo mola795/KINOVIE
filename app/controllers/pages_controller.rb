7a381835acc1ac181a822e2b7e7b38abee831b4a
Yoana Chang <yewonyoana@gmail.com>
2024-05-23 17:57:05 +0900

    Edited Homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @titles = Title.all.select { |title| title.name.split.size <= 3 }
   end
 end
