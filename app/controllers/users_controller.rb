851aaff0a7c0236dfd7d09d974de5820af45052b
vincent de leonardis <vincent.deleo@gmail.com>
2024-05-24 16:15:29 +0900

    small fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
 
   def show
-    # @user = User.find(params[:id])
-    # @lists = @user.lists.where.not(status: 'Private').order(:created_at)
-    # @private_lists = @user.lists.where(status: 'Private').where.not(name: 'Watchlist').order(:created_at)
-    # @watchlist = @user.lists.find_by(name: 'Watchlist')
-  end
-
-  def lists
     @user = User.find(params[:id])
     @lists = @user.lists.where.not(status: 'Private').order(:created_at)
     @private_lists = @user.lists.where(status: 'Private').where.not(name: 'Watchlist').order(:created_at)
     @watchlist = @user.lists.find_by(name: 'Watchlist')
+  end
+
+  def lists
+    # @user = User.find(params[:id])
+    # @lists = @user.lists.where.not(status: 'Private').order(:created_at)
+    # @private_lists = @user.lists.where(status: 'Private').where.not(name: 'Watchlist').order(:created_at)
+    # @watchlist = @user.lists.find_by(name: 'Watchlist')
   end
 
 end
